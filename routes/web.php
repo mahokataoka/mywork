91924234d26ca163720e8be784482b179e0cc2be
mahokataoka <20026059@mmm.muroran-it.ac.jp>
2022-12-03 11:58:29 +0900

    8-1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts/index');
 });
+Route::get('/posts', [PostController::class, 'index']);
