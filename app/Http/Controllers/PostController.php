3e1265a118bd5b14946f970c36df41ff559eb857
mahokataoka <20026059@mmm.muroran-it.ac.jp>
2022-12-03 12:30:44 +0900

    8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,8 @@ class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-    return $post->get();//$postの中身を戻り値にする。
+    //return view('posts/index')->with(['posts' => $post->getByLimit()]);
+    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
 }
 
