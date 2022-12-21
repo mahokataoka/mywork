0976e8f16f87f10e491c2ee6f5ffd8917cd26ca5
mahokataoka <20026059@mmm.muroran-it.ac.jp>
2022-12-22 00:06:02 +0900

    8-3 詳細画面

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,6 +18,6 @@ class Post extends Model
     public function getPaginateByLimit(int $limit_count = 10)
     {
     // updated_atで降順に並べたあと、limitで件数制限をかける
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
